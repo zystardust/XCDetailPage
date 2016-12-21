f3a94a13341b8055f1f021dc4d7e7515c6e436a6
张玉 <max.zero.yeah@gmail.com>
2016-12-21 15:15:03 +0800

    添加pod文件及第三方框架

---
+++ XCDetailPage/XCDetailPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XCDetailPage
+//
+//  Created by zhangyu on 2016/12/21.
+//  Copyright © 2016年 zhangyu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XCDetailPage/XCDetailPage/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XCDetailPage
+//
+//  Created by zhangyu on 2016/12/21.
+//  Copyright © 2016年 zhangyu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    
+}
+
+
+@end
